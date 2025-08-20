a19e47bf1aa928f5f620489751f1abb45d71a50e
Nikhil Nagnath Lawale <127715791+Nikhillawale07@users.noreply.github.com>
2025-08-20 18:17:56 +0530

    Update App.java

--- sbapi1/src/main/java/com/demo/App.java
+++ sbapi1/src/main/java/com/demo/App.java
@@ -1,5 +1,4 @@
 package com.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +7,4 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
 }
